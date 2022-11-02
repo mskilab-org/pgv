7f56258710a1ec04ec2c87f68f60c596c6e1bfea
C.Xanthopoulakis <c.xanthopoulakis@gmail.com>
2022-11-02 19:41:42 +0100

    add locator for genes in gene panel header

--- src/components/genesPanel/index.js
+++ src/components/genesPanel/index.js
@@ -4,14 +4,17 @@ import { connect } from "react-redux";
 import { withTranslation } from "react-i18next";
 import ContainerDimensions from "react-container-dimensions";
 import handleViewport from "react-in-viewport";
-import { Card, Space, Tooltip, Button, message, Row, Col } from "antd";
+import { Card, Space, Tooltip, Button, message, Row, Col, Select } from "antd";
 import * as d3 from "d3";
 import { AiOutlineDownload } from "react-icons/ai";
-import { downloadCanvasAsPng } from "../../helpers/utility";
+import { downloadCanvasAsPng, merge, cluster } from "../../helpers/utility";
 import * as htmlToImage from "html-to-image";
 import { CgArrowsBreakeH } from "react-icons/cg";
 import Wrapper from "./index.style";
 import GenesPlot from "../genesPlot";
+import appActions from "../../redux/app/actions";
+
+const { updateDomains } = appActions;
 
 const margins = {
   padding: 0,
@@ -39,9 +42,48 @@ class GenesPanel extends Component {
       });
   };
 
+  handleGenesLocatorChange = (values) => {
+    const { genomeLength, chromoBins, genes } = this.props;
+    let selectedGenes = values.map((d, i) => genes.get(d).toJSON());
+    let newDomains = selectedGenes.map((d, i) => [
+      d3.max([Math.floor(0.99999 * d.startPlace), 1]),
+      d3.min([Math.floor(1.00001 * d.endPlace), genomeLength]),
+    ]);
+    if (values.length < 1) {
+      let firstChromosome = Object.values(chromoBins)[0];
+      newDomains = [[firstChromosome.startPlace, firstChromosome.endPlace]];
+      this.props.updateDomains(newDomains);
+    } else {
+      let merged = merge(
+        newDomains
+          .map((d) => {
+            return { startPlace: d[0], endPlace: d[1] };
+          })
+          .sort((a, b) => d3.ascending(a.startPlace, b.startPlace))
+      );
+      this.props.updateDomains(cluster(merged, genomeLength));
+    }
+  };
+
   render() {
     const { t, genes, domains } = this.props;
     if (!genes) return null;
+    let geneTypesIndexes = genes
+      .getChild("type")
+      .toArray()
+      .map((d, i) => (d === "gene" ? i : undefined))
+      .filter((x) => x);
+    let geneTitlesList = genes.getChild("title").toArray();
+    let optionsList = geneTypesIndexes
+      .map((d, i) => {
+        return {
+          label: geneTitlesList[d],
+          value: d,
+        };
+      })
+      .sort((a, b) =>
+        d3.ascending(a.label.toLowerCase(), b.label.toLowerCase())
+      );
     return (
       <Wrapper>
         {
@@ -63,6 +105,26 @@ class GenesPanel extends Component {
             }
             extra={
               <Space>
+                <Select
+                  allowClear
+                  showSearch
+                  mode="multiple"
+                  style={{ width: 300 }}
+                  placeholder={t("components.genes-panel.locator")}
+                  onChange={this.handleGenesLocatorChange}
+                  options={optionsList}
+                  optionFilterProp="children"
+                  filterOption={(input, option) =>
+                    (option?.label.toLowerCase() ?? "").includes(
+                      input.toLowerCase()
+                    )
+                  }
+                  filterSort={(optionA, optionB) =>
+                    (optionA?.label ?? "")
+                      .toLowerCase()
+                      .localeCompare((optionB?.label ?? "").toLowerCase())
+                  }
+                />
                 <Tooltip title={t("components.download-as-png-tooltip")}>
                   <Button
                     type="default"
@@ -108,10 +170,14 @@ class GenesPanel extends Component {
 }
 GenesPanel.propTypes = {};
 GenesPanel.defaultProps = {};
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  updateDomains: (domains) => dispatch(updateDomains(domains)),
+});
 const mapStateToProps = (state) => ({
+  chromoBins: state.App.chromoBins,
   domains: state.App.domains,
   renderOutsideViewPort: state.App.renderOutsideViewPort,
+  genomeLength: state.App.genomeLength,
 });
 export default connect(
   mapStateToProps,
